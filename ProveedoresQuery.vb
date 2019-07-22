f00b61498d044bf921e3f4f27b683a4671f88e56
cone011 <32248132+cone011@users.noreply.github.com>
2019-07-22 15:37:15 -0400

    Add files via upload

--- ProveedoresQuery.vb
+++ ProveedoresQuery.vb
@@ -1,6 +1,8 @@
 
 Public Class ProveedoresQuery
 	
+	Public Property isNew As Boolean = False
+	
 	Public ReadOnly Property GetAll() As String 
 		Get
 			Dim resultado As String = String.Empty
@@ -33,6 +35,22 @@ Public Class ProveedoresQuery
 			Catch
 				Throw
 			End Try
+		End Get
+	End Property
+	
+	Public ReadOnly Property SaveProveedor() As String
+		Get
+			Dim resultado As String = String.Empty
+			Try
+				If isNew Then 
+					resultado = "INSERT INTO proveed(codpro, nompro, ruc, tele1) VALUES (@CodigoProveedor, @NombreProveedor, @RazonSocialProveedor, @TelefonoProveedor)"
+				Else 
+					resultado = "UPDATE proveed SET codpro = @CodigoProveedor, nompro = @NombreProveedor, ruc = @RazonSocialProveedor, tel1 = @TelefonoProveedor"
+				End If
+			Catch
+				Throw
+			End Try
+			Return resultado
 		End Get
 	End Property
 End Class
