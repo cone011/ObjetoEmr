7bb31b2a56327e2461499ed9f3f22900c541d496
cone011 <32248132+cone011@users.noreply.github.com>
2019-07-22 15:50:57 -0400

    Add files via upload

--- TipoProveedorQuery.vb
+++ TipoProveedorQuery.vb
@@ -2,6 +2,8 @@
 
 Public Class TipoProveedorQuery 
 	
+	Public Property isNew As Boolean = False
+	
 	Public ReadOnly Property GetAll() As String 
 		Get
 			Dim resultado As String = String.Empty
@@ -19,6 +21,22 @@ Public Class TipoProveedorQuery
 			Dim resultado As String = String.Empty
 			Try
 				resultado = "SELECT * FROM TipoProveedor WHERE idTipoProveedor = @idTipoProveedor"
+			Catch
+				Throw
+			End Try
+			Return resultado
+		End Get
+	End Property
+	
+	Public ReadOnly Property SaveTipoProveedor() As String 
+		Get
+			Dim resultado As String = String.Empty
+			Try
+				If isNew Then 
+					resultado = "INSERT INTO TipoProveedor(CodigoTipoProveedor, DescripcionTipoProveedor, FechaCreacion, UsuarioCreacion) VALUES (@CodigoTipoProveedor, @DescripcionTipoProveedor, CURDATE(), @UsuarioCreacion)"
+				Else 
+					resultado = "UPDATE TipoProveedor SET CodigoTipoProveedor = @CodigoTipoProveedor, DescripcionTipoProveedor = @DescripcionTipoProveedor, UsuarioModificacion = @UsuarioModificacion, FechaModificacion = CURDATE()"
+				End If
 			Catch
 				Throw
 			End Try
